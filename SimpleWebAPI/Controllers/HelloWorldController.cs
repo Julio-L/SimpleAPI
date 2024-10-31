b3cebfc6077294870f586d502fbd59dfe93c62c2
Julio Lima <lima.julio@yahoo.com>
2024-10-31 19:33:00 -0400

    test

---
+++ SimpleWebAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Message()
+        {
+            return "Hello, World!";
+        }
+
+
+    }
+}
